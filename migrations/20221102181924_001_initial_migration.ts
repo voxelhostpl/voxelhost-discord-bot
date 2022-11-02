427ac3875dcbe1d578a1f085db9a03124a64953c
iipanda <iipandarx@protonmail.com>
2022-11-02 20:18:18 +0100

    change migration

--- migrations/20221102181924_001_initial_migration.ts
+++ migrations/20221102181924_001_initial_migration.ts
@@ -1,25 +1,21 @@
 import { Knex } from "knex";
 
 export async function up(knex: Knex): Promise<void> {
-  await knex.raw(`
-    CREATE TABLE IF NOT EXISTS customers (
-      discordId TEXT PRIMARY KEY
-    );
-    
-    CREATE TABLE IF NOT EXISTS suggestions (
-      messageId    TEXT PRIMARY KEY,
-      status       TEXT CHECK(status IN ('REJECTED', 'PENDING', 'APPROVED', 'DONE')),
-      authorName   TEXT,
-      authorAvatar TEXT,
-      timestamp    INTEGER,
-      content      TEXT
-    );
-  `);
+  knex.schema.createTableIfNotExists("customers", table => {
+    table.string("discordId").primary();
+  });
+
+  knex.schema.createTableIfNotExists("suggestions", table => {
+    table.string("messageId").primary();
+    table.string("status").checkIn(["REJECTED", "PENDING", "APPROVED", "DONE"]);
+    table.string("authorName");
+    table.string("authorAvatar");
+    table.integer("timestamp");
+    table.string("content");
+  });
 }
 
 export async function down(knex: Knex): Promise<void> {
-  await knex.raw(`
-    DROP TABLE customers;
-    DROP TABLE suggestions;
-  `);
+  knex.schema.dropTableIfExists("customers");
+  knex.schema.dropTableIfExists("suggestions");
 }
